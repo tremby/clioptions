ed91f6de7a38d7def7b9bb5ae4550d9d93296e58
Bart Nagel <bart@tremby.net>
2011-01-24 23:38:23 +0000

    initial commit

---
+++ CliOption.class.php
@@ -0,0 +1,194 @@
+<?php
+
+/** CliOptions class set
+ * http://github.com/tremby/clioptions
+ *
+ * Copyright 2011 Bart Nagel (bart@tremby.net)
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** CliOption class
+ * This doesn't need to be directly interacted with, but its constructor is 
+ * directly invoked by the CliOptions class's add method.
+ */
+class CliOption {
+	const TYPE_SWITCH = 0;
+	const TYPE_ACCUMULATOR = 1;
+	const TYPE_VALUE = 2;
+	const TYPE_OPTIONALVALUE = 3;
+	const TYPE_MULTIPLEVALUE = 4;
+
+	private $short;
+	private $long;
+	private $type;
+	private $default;
+	private $helptext;
+
+	/** constructor
+	 * All arguments are optional and are skipped by giving null values, though 
+	 * at least one of the first two arguments must be given.
+	 * The arguments:
+	 *	$short
+	 *		The option's short name
+	 *	$long
+	 *		The option's long name
+	 *	$type
+	 *		One of the TYPE_* constants defined in this class
+	 *	$default
+	 *		The default value for the option -- this should seldom need to be 
+	 *		overridden
+	 *	$helptext
+	 *		Some help text to be output alongside the option's forms, type and default 
+	 *		when CliOptions::showopts() is called
+	 *
+	 * This method is fairly picky about the datatypes it is given.
+	 */
+	public function __construct($short = null, $long = null, $type = self::TYPE_SWITCH, $default = null, $helptext = null) {
+		if (!is_null($short) && (!is_string($short) || !preg_match('%^[a-zA-Z0-9]$%', $short)))
+			die("CliOption: short option must be one alphanumeric character");
+		$this->short = $short;
+
+		if (!is_null($long) && (!is_string($long) || !preg_match('%^[a-zA-Z0-9][a-zA-Z0-9-]+$%', $long)))
+			die("CliOption: long option must be a string at least two characters long, the first of which is alphanumeric, the rest of which also allowing hyphens");
+		$this->long = $long;
+
+		if (is_null($short) && is_null($long))
+			die("CliOption: option must have at least one of short and long forms");
+
+		switch ($type) {
+			case self::TYPE_SWITCH:
+				if (is_null($this->default))
+					$default = false;
+				else if (!is_bool($default))
+					die("CliOption: default must be a boolean for switch type, or null to keep the basic default");
+				break;
+			case self::TYPE_ACCUMULATOR:
+				if (is_null($this->default))
+					$default = 0;
+				else if (!is_int($default))
+					die("CliOption: default must be zero or a positive integer for accumulator type, or null to keep the basic default");
+				break;
+			case self::TYPE_VALUE:
+				if (is_null($default))
+					$default = false;
+				else if (!is_string($default))
+					die("CliOption: default must be a string for value type, or null to keep the basic default");
+				break;
+			case self::TYPE_OPTIONALVALUE:
+				if (is_null($default))
+					$default = false;
+				else if (!is_bool($default) && !is_string($default))
+					die("CliOption: default must be a boolean or a string for optional value type, or null to keep the basic default");
+				break;
+			case self::TYPE_MULTIPLEVALUE:
+				$errormessage = "CliOption: default must be an array (which could be empty) of string values for multiple value type, or null to keep the basic default";
+				if (is_null($default))
+					$default = array();
+				else if (!is_array($default))
+					die($errormessage);
+				foreach ($default as $value)
+					if (!is_string($value))
+						die($errormessage);
+				break;
+			default:
+				die("CliOption: one of the CliOption::TYPE_* constants should be used for type");
+		}
+		$this->type = $type;
+		$this->default = $default;
+
+		$this->helptext = $helptext;
+	}
+
+	/** short
+	 * Return the option's short name
+	 */
+	public function short() {
+		return $this->short;
+	}
+
+	/** long
+	 * Return the option's long name
+	 */
+	public function long() {
+		return $this->long;
+	}
+
+	/** type
+	 * Return the option's type
+	 */
+	public function type() {
+		return $this->type;
+	}
+
+	/** def
+	 * Return the option's default value
+	 */
+	public function def() {
+		return $this->default;
+	}
+
+	/** helptext
+	 * Return the object's help text
+	 */
+	public function helptext() {
+		return $this->helptext;
+	}
+
+	/** hasshort
+	 * Return true if the option has a short name
+	 */
+	public function hasshort() {
+		return !is_null($this->short);
+	}
+
+	/** haslong
+	 * Return true if the option has a long name
+	 */
+	public function haslong() {
+		return !is_null($this->long);
+	}
+
+	/** longest
+	 * Return the option's long name if it has one, otherwise its short name
+	 */
+	public function longest() {
+		if ($this->haslong())
+			return $this->long();
+		return $this->short();
+	}
+
+	/** cantakearg
+	 * Return true if the option can take an argument
+	 */
+	public function cantakearg() {
+		return in_array($this->type(), array(
+			self::TYPE_VALUE,
+			self::TYPE_OPTIONALVALUE,
+			self::TYPE_MULTIPLEVALUE,
+		));
+	}
+
+	/** musttakearg
+	 * Return true if the option requires an argument
+	 */
+	public function musttakearg() {
+		return in_array($this->type(), array(
+			self::TYPE_VALUE,
+			self::TYPE_MULTIPLEVALUE,
+		));
+	}
+}
+
+?>
---
+++ CliOptions.class.php
@@ -0,0 +1,292 @@
+<?php
+
+/** CliOptions class set
+ * http://github.com/tremby/clioptions
+ *
+ * Copyright 2011 Bart Nagel (bart@tremby.net)
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require_once "CliOptionException.class.php";
+require_once "CliOptionTakesNoArgumentException.class.php";
+require_once "CliOptionRequiresArgumentException.class.php";
+require_once "CliOptionNotFoundException.class.php";
+require_once "CliOption.class.php";
+
+/** CliOptions class
+ * This is the only class which needs to be directly interacted with, though the 
+ * arguments for the constructor of CliOption must be used for the add method 
+ * and its constants must also be used.
+ * This class is instantiated and given a source array of commandline arguments. 
+ * This defaults to $_SERVER["argv"].
+ */
+class CliOptions {
+	private $options;
+	private $source;
+
+	/** constructor
+	 * Optionally takes an array of command line arguments, but uses 
+	 * $_SERVER["argv"] by default
+	 */
+	public function __construct($source = null) {
+		if (is_null($source))
+			$source = $_SERVER["argv"];
+
+		if (!is_array($source))
+			die("CliOptions: source must be an array");
+		$this->source = $source;
+
+		$this->options = array();
+	}
+
+	/** add
+	 * Add an option -- takes the arguments of the CliOption constructor and 
+	 * returns this CliOption object so calls can be chained together
+	 */
+	public function add() {
+		$args = func_get_args();
+		$reflection = new ReflectionClass("CliOption");
+		$option = $reflection->newInstanceArgs($args);
+		$this->options[] = $option;
+		return $this;
+	}
+
+	/** showopts
+	 * Print out each option and its type, default and any help text
+	 */
+	public function showopts() {
+		foreach ($this->options as $option) {
+			if ($option->hasshort()) {
+				if ($option->musttakearg()) {
+					echo "-" . $option->short() . "<ARG>\n";
+					echo "-" . $option->short() . " <ARG>\n";
+				} else if ($option->cantakearg())
+					echo "-" . $option->short() . "[<ARG>]\n";
+				else
+					echo "-" . $option->short() . "\n";
+			}
+			if ($option->haslong()) {
+				if ($option->musttakearg()) {
+					echo "--" . $option->long() . "=<ARG>\n";
+					echo "--" . $option->long() . " <ARG>\n";
+				} else if ($option->cantakearg())
+					echo "--" . $option->long() . "[=<ARG>]\n";
+				else
+					echo "--" . $option->long() . "\n";
+			}
+			echo "\t";
+			switch ($option->type()) {
+				case CliOption::TYPE_SWITCH:
+					echo "switch; default value is " . ($option->def() ? "true" : "false");
+					break;
+				case CliOption::TYPE_ACCUMULATOR:
+					echo "accumulating switch; default value is " . $option->def();
+					break;
+				case CliOption::TYPE_VALUE:
+					echo "option requiring a value; " . ($option->def() === false ? "no default value" : "default value is '" . $option->def() . "'");
+					break;
+				case CliOption::TYPE_OPTIONALVALUE:
+					echo "option with an optional value; default value is " . ($option->def() === false ? "unset" : ($option->def() === true ? "set" : "'" . $option->def() . "'"));
+					break;
+				case CliOption::TYPE_MULTIPLEVALUE:
+					echo "requires a value and can be used multiple times; ";
+					if (count($option->def()) == 0)
+						echo "no default values";
+					else
+						echo "default values are '" . implode("', '", $option->def()) . "'";
+					break;
+			}
+			echo "\n";
+			if (strlen($option->helptext()))
+				echo "\t" . $option->helptext() . "\n";
+		}
+	}
+
+	// return true if there are more arguments left to process
+	private function moreargs() {
+		return count($this->source) > 0;
+	}
+
+	// return the next argument and remove it from the source array
+	private function getarg() {
+		if (!$this->moreargs())
+			die("CliOptions: tried to get an argument but there were none left -- should be using CliOptions::moreargs()");
+		return array_shift($this->source);
+	}
+
+	// prepend an argument to the source array
+	private function prependarg($arg) {
+		array_unshift($this->source, $arg);
+	}
+
+	/** getopts
+	 * Parse the commandline arguments in the source array and return an 
+	 * associative array
+	 * Any non-option arguments are in an array given by the "_" index, in the 
+	 * order in which they appeared on the commandline.
+	 * Other indices are for each option -- the index is the long option name if 
+	 * it exists, otherwise the short option.
+	 * The values depend on the option type: boolean for switch, integer for an 
+	 * accumulator, string or false (false meaning the option wasn't given) for 
+	 * an option requiring an argument, string or boolean for an option with an 
+	 * optional argument (false meaning the option wasn't given, true meaning it 
+	 * was but no argument was given) and an array of strings in the order in 
+	 * which they were given on the commandline for an option which can take 
+	 * multiple values.
+	 */
+	public function getopts() {
+		$nomoreoptions = false;
+
+		// set all defaults
+		$options = array(
+			"_" => array(),
+		);
+		foreach ($this->options as $option)
+			$options[$option->longest()] = $option->def();
+
+		while ($this->moreargs()) {
+			$arg = $this->getarg();
+
+			if (!$nomoreoptions && $arg == "--") {
+				$nomoreoptions = true;
+				continue;
+			}
+
+			if ($nomoreoptions) {
+				$options["_"][] = $arg;
+				continue;
+			}
+
+			if (substr($arg, 0, 2) == "--") {
+				// long option
+				$key = substr($arg, 2);
+
+				// some long arguments can take an argument
+				$value = null;
+
+				// an argument can be directly afterwards, separated by an =
+				// if the argument is optional it must be given this way
+				if (strpos($key, "=") !== false)
+					list($key, $value) = explode("=", $key, 2);
+
+				// find the option the user meant
+				$option = $this->getoptionfromlong($key);
+
+				// if we have a value already but the option can't take one 
+				// throw an error
+				if (!is_null($value) && !$option->cantakearg())
+					throw new CliOptionTakesNoArgumentException("Option '--" . $option->long() . "' cannot take an argument");
+
+				// if the option must take an argument it could also be given 
+				// afterwards, so if it's required and we don't have one yet we 
+				// need to grab it
+				if ($option->musttakearg() && is_null($value)) {
+					if (!$this->moreargs())
+						throw new CliOptionRequiresArgumentException("Option '--" . $option->long() . "' requires an argument");
+					$value = $this->getarg();
+				}
+			} else if ($arg[0] == "-") {
+				// short option letter
+				$key = $arg[1];
+
+				// there may be more options or a value
+				$remainder = substr($arg, 2);
+
+				// find the option
+				$option = $this->getoptionfromshort($key);
+
+				// if there are more characters, this is an argument if the 
+				// option supports one or otherwise it's more options
+				// if there are no more characters, look for an argument only if 
+				// the option requires one (if it's optional it needs to be 
+				// given directly after the option letter)
+				$value = null;
+				if (strlen($remainder) > 0) {
+					if ($option->cantakearg())
+						$value = $remainder;
+					else
+						$this->prependarg("-" . $remainder);
+				} else if ($option->musttakearg()) {
+					if (!$this->moreargs())
+						throw new CliOptionRequiresArgumentException("Option '-" . $option->short() . "' requires an argument");
+					$value = $this->getarg();
+				}
+			} else {
+				$options["_"][] = $arg;
+				continue;
+			}
+
+			switch ($option->type()) {
+				case CliOption::TYPE_SWITCH:
+					$options[$option->longest()] = true;
+					break;
+				case CliOption::TYPE_ACCUMULATOR:
+					$options[$option->longest()]++;
+					break;
+				case CliOption::TYPE_VALUE:
+				case CliOption::TYPE_OPTIONALVALUE:
+					$options[$option->longest()] = is_null($value) ? true : $value;
+					break;
+				case CliOption::TYPE_MULTIPLEVALUE:
+					$options[$option->longest()][] = $value;
+					break;
+			}
+		}
+
+		return $options;
+	}
+
+	// return the CliOption object corresponding to the given long option name, 
+	// searching for a unique expansion if an exact match isn't found
+	private function getoptionfromlong($key) {
+		// look for exact match
+		foreach ($this->options as $option) {
+			if (!$option->haslong())
+				continue;
+			if ($option->long() == $key)
+				return $option;
+		}
+
+		// look for non-ambiguous abbreviation
+		$abbreviations = array();
+		foreach ($this->options as $option) {
+			if (!$option->haslong())
+				continue;
+			if (strpos($option->long(), $key) === 0)
+				$abbreviations[] = $option;
+		}
+		if (count($abbreviations) == 1)
+			return $abbreviations[0];
+		if (count($abbreviations) == 0)
+			throw new CliOptionNotFoundException("Long option '--$key' doesn't exist, nor does it abbreviate any existing long option");
+		$fullnames = array();
+		foreach ($abbreviations as $option)
+			$fullnames[] = $option->long();
+		throw new CliOptionNotFoundException("Long option '--$key' doesn't exist but is an ambiguous abbreviation of '" . implode("', '", $fullnames) . "'");
+	}
+
+	// return the CliOption object corresponding to the given short option name
+	private function getoptionfromshort($key) {
+		foreach ($this->options as $option) {
+			if (!$option->hasshort())
+				continue;
+			if ($option->short() == $key)
+				return $option;
+		}
+		throw new CliOptionNotFoundException("Short option '-$key' doesn't exist");
+	}
+}
+
+?>
